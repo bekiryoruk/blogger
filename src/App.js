62463b7fe35f6a352082622ce2937bc5fb2ecd96
Bekir Yörük <bekiryoruk1@gmail.com>
2021-03-27 16:17:29 +0300

    adding routers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import './App.css'
 import Login from './Login.js'
+import Navbar from './Navbar.js'
+import Post from './Post.js'
+import About from './About.js'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Home from './Home'
 
 function App() {
   return (
-    <div>
-      <Login />
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/post' component={Post} />
+          <Route path='/login' component={Login} />
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
---
+++ src/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+function Navbar() {
+  return (
+    <nav>
+      <Link to='/'>
+        <h3>blogger</h3>
+      </Link>
+
+      <ul className='navlinks'>
+        <Link to='/about'>
+          <li>About</li>
+        </Link>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
