ae98e043df9e7f4610f045e4e3e738714976daf7
Bekir Yörük <bekiryoruk1@gmail.com>
2021-03-29 11:19:39 +0300

    deleting unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import './App.css'
-import Login from './Login.js'
 import Navbar from './Navbar.js'
 import Post from './Post.js'
 import About from './About.js'
@@ -8,23 +7,14 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './Home'
 
 function App() {
-  const posts = [
-    { id: 1, title: 'Hello World', content: 'Welcome to learning React!' },
-    {
-      id: 2,
-      title: 'Installation',
-      content: 'You can install React from npm.',
-    },
-  ]
   return (
     <Router>
       <div>
         <Navbar />
         <Switch>
-          <Route path='/' exact component={() => <Home posts={posts} />} />
+          <Route path='/' exact component={() => <Home />} />
           <Route path='/about' component={About} />
           <Route path='/post' component={Post} />
-          <Route path='/login' component={Login} />
         </Switch>
       </div>
     </Router>
--- src/Home.js
+++ src/Home.js
@@ -2,16 +2,8 @@ import React from 'react'
 import Post from './Post'
 import UserInfo from './UserInfo'
 function Home(props) {
-  const content = props.posts.map((post) => (
-    <div key={post.id}>
-      <h3>{post.title}</h3>
-      <p>{post.content}</p>
-    </div>
-  ))
   return (
     <div className='home'>
-      {content}
-
       <div>
         <UserInfo />
       </div>
